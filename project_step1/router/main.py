d48ccc1af666598efdead7259de4f3f135adf5c4
Alireza Amini Jezeh <alrezamini@yahoo.com>
2024-10-03 11:20:17 +0330

    test function defined with /api/test/{number} endpoint (post request allowed) and BodyInput class which validates the data type of body request defined (should be a list of objects)

--- project_step1/router/main.py
+++ project_step1/router/main.py
@@ -1,3 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, Body
+from typing import Annotated
+from pydantic import BaseModel
+
 
 router = APIRouter()
+
+
+class BodyInput(BaseModel):
+    BodyId: str
+    BodyIdNumber: str
+    name: str
+
+
+@router.post("/test/{number}")
+def test(
+    number: int,
+    id: Annotated[str, Header()],
+    input_body: Annotated[list[BodyInput], Body()]
+):
+    return input_body
