5aa521d204511c94a536f0df727bc4ecfe5cc2cb
Alireza Amini Jezeh <alrezamini@yahoo.com>
2024-10-03 10:47:42 +0330

    router included with /api prefix (prefix keyword argument added to include_router function)

--- project_step1/main.py
+++ project_step1/main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 from router.main import router as main_router
 
 main_app = FastAPI()
-main_app.include_router(main_router)
+main_app.include_router(main_router, prefix="/api")
